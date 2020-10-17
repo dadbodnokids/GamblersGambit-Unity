e7c7414af1942a803878365b68715d5d1afd5f3b
Tanner B <tbrownoff@gmail.com>
2020-10-17 11:19:44 -0600

    First Video Progress
    Yeeee

---
+++ Assets/Scripts/DeskScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskScript : MonoBehaviour
+{
+    public Sprite[] cardSprites;
+    int[] cardValues = new int[53];
+    int currentIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void GetCardValues()
+    {
+        int num = 0;
+        // Loop to assign values to cards
+        for (int i = 0; i < cardSprites.Length; i++)
+        {
+            num = i;
+            // Count up to the amount of cards, 52
+            num %= 13;
+            // if there is a remainder after x/13, then remaider
+            // is used as the value, unless over 10, then use 10
+            if(num > 10 || num == 0)
+            {
+                num = 10
+            }
+            cardValues[i] = num++;
+        }
+        currentIndex = 1;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //Game Buttons
+    public Button dealBtn;
+    public Button hitBtn;
+    public Button standBtn;
+    public Button betBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Add on click listeners to the buttons
+        dealBtn.onClick.AddListener(() => DealClicked());
+        hitBtn.onClick.AddListener(() => HitClicked());
+        standBtn.onClick.AddListener(() => StandClicked());
+
+    }
+
+    private void DealClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HitClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StandClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+}
