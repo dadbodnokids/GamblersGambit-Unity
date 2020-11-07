0c1efb91820286fb24ab023cadf831da34867533
Tanner B <tbrownoff@gmail.com>
2020-11-07 12:11:18 -0700

    Scratch Card Base
    Made a scratch card with 1 box that can be scratched

---
+++ Assets/Scripts/ScratchOff.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScratchOff : MonoBehaviour
+{
+    public GameObject maskPrefab;
+    private bool isPressed = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        mousePos.z = 2;
+
+        if (isPressed == true)
+        {
+            GameObject maskSprite = Instantiate(maskPrefab, mousePos, Quaternion.identity);
+            maskSprite.transform.parent = gameObject.transform;
+        }
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Invoke("reveal", 10);
+            isPressed = true;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isPressed = false;
+        }
+        
+    }
+
+    void reveal()
+    {
+        Destroy(this.gameObject);
+    }
+}
