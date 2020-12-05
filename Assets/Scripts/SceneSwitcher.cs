661f86d0925cc4e0bf5202a52957897fd5fd7daa
Scott Mattes <scottmattes90@gmail.com>
2020-12-05 14:37:43 -0700

    Final touches pt. 1
    Added end screens, added music & button click sound effects

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -11,8 +11,28 @@ public class SceneSwitcher : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("ScratcherMenu");
+    }
+
     public void PreviousScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void GoodEnding()
+    {
+        SceneManager.LoadScene("ScratcherEnd_good");
+    }
+
+    public void BadEnding()
+    {
+        SceneManager.LoadScene("ScratcherEnd_bad");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
